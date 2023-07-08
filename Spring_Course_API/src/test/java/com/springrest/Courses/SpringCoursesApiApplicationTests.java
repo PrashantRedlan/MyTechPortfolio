4205bf6c5e0c441d9858d7b1aa439fd1c9a6dbae
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2023-07-08 13:33:41 +0530

    Basic Spring Operations

---
+++ Spring_Course_API/src/test/java/com/springrest/Courses/SpringCoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.Courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
